a38c3804e26492f7825283fb1f836362dd4931ce
Rene1814 <rne_souza@yahoo.com.br>
2023-02-26 16:42:56 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,7 @@ public class Program {
 		//É tbm uma forma de fazer a injeção de dependência sem explicitar a implementação
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
